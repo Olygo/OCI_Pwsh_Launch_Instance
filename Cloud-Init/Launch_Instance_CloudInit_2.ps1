a73f991c3b7582a8c464fd41963d987d5f01475e
Florian Bonneville <25223006+Olygo@users.noreply.github.com>
2025-01-29 00:44:13 +0100

    Update Launch_Instance_CloudInit_2.ps1

--- Cloud-Init/Launch_Instance_CloudInit_2.ps1
+++ Cloud-Init/Launch_Instance_CloudInit_2.ps1
@@ -17,13 +17,16 @@ Set-TimeZone -Id "Romance Standard Time"
 Set-WinUserLanguageList -LanguageList fr-FR -Force
 Set-Culture fr-FR
 
-# install Windows Update Modules 
-Get-PSRepository
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module -Name PSWindowsUpdate -Force -AllowClobber
-Import-Module PSWindowsUpdate
-Get-WindowsUpdate -MicrosoftUpdate -Verbose
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot -Verbose
+# Enable Remote Desktop
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+Add-LocalGroupMember -Group "Remote Desktop Users" -Member "Administrator"
+Add-LocalGroupMember -Group "Remote Desktop Users" -Member "opc"
+
+# Disable IE Protection mode for Administators
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{3AF3F5DC-50F4-4F8C-848E-F4E3A7A91DB6}" -Name "IsInstalled" -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A3DD5F92-658A-410F-84FD-6FBB0B7B0262}" -Name "IsInstalled" -Value 0
+
 # Show hidden files and folders
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name Hidden -Value 1
 # Show file extensions
@@ -32,6 +35,14 @@ Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name ShowDrivesWithNoVolumeLabel -Value 1
 # Show protected operating system files
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name ShowSuperHidden -Value 1
+
+# install Windows Update Modules 
+Get-PSRepository
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module -Name PSWindowsUpdate -Force -AllowClobber
+Import-Module PSWindowsUpdate
+Get-WindowsUpdate -MicrosoftUpdate -Verbose
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot -Verbose
 
 "@
 
